bfd55749f889e236b88478eba8b78d8b02cd1e26
RazanAl-Quran <quraan_razan@hotmail.com>
2025-11-27 15:59:12 +0300

    feat: react part 01 & 02 done

---
+++ 03-React/part-01-02/src/Greeting.jsx
@@ -0,0 +1,8 @@
+// function Greeting(props) {
+function Greeting({username,username2}) {
+    // return <h2>Hello, {props.username} 👋</h2>;
+    return <h2>Hello, {username2} 👋</h2>;
+  }
+  
+  export default Greeting;
+  
